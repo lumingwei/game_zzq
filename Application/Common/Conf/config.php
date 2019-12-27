032667dc2d26dbf4fcd87268eaee71b466b62005
卢明炜 <307958040@qq.com>
2019-12-27 19:49:41 +0800

    数据库配置

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,11 +2,11 @@
 return array(
 	//'配置项'=>'配置值'
     'DB_TYPE'=>'mysql',      //数据库类型
-    'DB_HOST'=>'176.122.166.164',  //服务器地址
-    'DB_NAME'=>'sow',       //数据库名
+    'DB_HOST'=>'111.229.216.178',  //服务器地址
+    'DB_NAME'=>'qyn',       //数据库名
     'DB_USER'=>'root',       //数据库账号
-    'DB_PWD'=>'123456@!testS',      //数据库密码
+    'DB_PWD'=>'@Lumingwei666',      //数据库密码
     'DB_PORT'=>3306,         //数据库端口
-    'DB_PREFIX'=>'sow_',      //数据库表前缀
+    'DB_PREFIX'=>'qyn_',      //数据库表前缀
     'DB_CHARSET'=>'utf8',    //数据库字符编码
 );
